c57b368b45eb6a1b4a86dded818ff95fa3ad3832
Ponn-Star <an.ht.62cntt@gmail.com>
2025-06-03 01:12:53 +0700

    change

---
+++ .history/BackEnd/models/Booking_20250526020844.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  user: {type: String, ref: "User", require: true},
+  room: {type: String, ref: "Room", require: true},
+  checkInDate: {type: Date, require: true},
+  checkOutDate: {type: Date, require: true},
+  totalPrice: {type: Number, require: true},
+  user: {
+    type: String,
+    enum: ["Chưa kiểm duyệt", "Đã xác nhận", "Đã hũy" ],
+    default: "Chưa kiểm duyệt",
+  },
+  paymentMethod: {
+    type: String,
+    require: true,
+    default: "Thanh toán trực tiếp"
+  },
+  isPaid: { type: Boolean, default: false}
+}, { timestamps: true });
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
---
+++ .history/FrontEnd/src/components/Loader_20250603004601.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useAppContext } from '../conext/AppContext'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const Loader = () => {
+
+    const { navigate } = useAppContext()
+    const { nextUrl } = useParams()
+
+    useEffect(()=>{
+        if(nextUrl) {
+            setTimeout(()=>{
+                navigate(`/${nextUrl}`)
+            },8000)
+        }
+    },[nextUrl])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Loader
